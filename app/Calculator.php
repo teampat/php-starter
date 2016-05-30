ead69191771c51fc40803e504d9f4b104eb1eac2
teampat <teampat@gmail.com>
2016-05-30 17:15:12 +0700

    First commit

---
+++ app/Calculator.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Cal;
+
+class Calculator {
+
+	public function add($x, $y){
+		return $x + $y;
+	}
+
+	public function diff($x, $y){
+		return $x - $y;
+	}
+}
---
+++ test/CalculatorTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+use Cal\Calculator;
+
+class CalculatorTest extends PHPUnit_Framework_TestCase {
+
+	protected $calc;
+
+	protected function setUp(){
+		$this->calc = new Calculator;
+	}
+
+	public function testAddNumber(){
+		$this->assertEquals(4,$this->calc->add(2,2)); 
+	}
+
+	public function testDiffNumber(){
+		$this->assertEquals(0,$this->calc->diff(2,2));
+	}
+
+}
